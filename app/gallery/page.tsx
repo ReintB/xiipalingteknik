6c4d681e220b1f7853866899716a29ab38f58b1b
ReintB <reintbarus@gmail.com>
2025-03-21 09:03:19 +0700

    feat: galeri/video done, change layout to 5 cols

--- app/gallery/page.tsx
+++ app/gallery/page.tsx
@@ -47,7 +47,7 @@ export default function GalleryPage() {
           </div>
         </TabsContent>
         <TabsContent value="videos">
-          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
+          <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-5 gap-6">
             {videos.map((video) => (
               <video
                 key={video.id}
